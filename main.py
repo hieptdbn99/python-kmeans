e294d1cf1f090d6fd27f63492b8e564bb63c4553
hieptdbn99 <tdhmesut@gmail.com>
2022-02-15 17:18:57 +0700

    update

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from pprint import pprint
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn as sklearn
+
+from sklearn.cluster import KMeans
 
 from scipy.spatial.distance import cdist
 
@@ -11,13 +14,14 @@ def main():
     # Importing the dataset
     dataset = pd.read_csv('D:\Downloads\Sales_Transactions_Dataset_Weekly.csv')
     #get column 50 to 53
-    X = dataset.iloc[:, 50:53].values
+    X = dataset.iloc[:, 55:58].values
+    print(X)
     #declare stop condition to use in calculateNewCluster
-    stop_condition = 0.01
+
     
     #elbow method to find the optimal number of clusters
-    Elbow(12, X)
-
+    Kmean(len(X),X,6)
+    process_test(len(X),X,6)
 
 
 def Elbow(range_k, listObj):
@@ -78,6 +82,33 @@ def calculateNewCluster(c, listObj, k):
             flag = False
     return c,flag,cluster
 
+
+
+
+def writeElementCluster(listObj, pred_label, center_point, K):
+    f = open('kmean_test.output', 'w')
+    #print cluster
+    # for i in range(len(listObj)):
+    #     f.write(str(pred_label[i])+'\n')
+    #print center point
+    for i in range(K):
+        f.write(str(center_point[i])+'\n')
+    f.close()
+
+
+
+def process_test(n,listObj,K):
+    kmeans_sklearn = KMeans(n_clusters=K, random_state=0).fit(listObj)
+    center_point = kmeans_sklearn.cluster_centers_
+    pred_label = kmeans_sklearn.predict(listObj)
+    #plt show Oxyz
+    # plt.scatter(listObj[:, 0], listObj[:, 1], c=pred_label, s=50, cmap='viridis')
+    # plt.scatter(center_point[:, 0], center_point[:, 1], c='black', s=200, alpha=0.5)
+    # plt.show()
+    plt.show()
+
+
+    writeElementCluster(listObj,pred_label,center_point,K)
 
 
 
